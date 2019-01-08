c17f5f711729ceaa0426101e928576071bcfa675
Ruchika Gupta <ruchika.gupta@nxp.com>
2019-01-08 11:11:40 +0530

    nxp/ls1046: Add support for frwy board
    Platform target added for ls1046afrwy board.
    DDR driver is not present in this platform currently.
    
    Signed-off-by: Ruchika Gupta <ruchika.gupta@nxp.com>

---
+++ plat/nxp/soc-ls1046/ls1046afrwy/ddr_init.c
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2019 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#include <platform_def.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <debug.h>
+#include <errno.h>
+#include <string.h>
+#include <io.h>
+#include <ddr.h>
+#include <utils.h>
+#include <utils_def.h>
+#include <errata.h>
+
+long long _init_ddr(void)
+{
+	long long dram_size = 0;
+
+	return dram_size;
+}
