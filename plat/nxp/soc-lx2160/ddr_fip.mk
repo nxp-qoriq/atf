75c8d19d073c12255df90c17910849a777486728
Pankaj Gupta <pankaj.gupta@nxp.com>
2020-06-30 14:35:11 +0530

    ddr-fip.mk changes to enable user to provide path
    ddr-fip.mk changes to enable user to provide path for
    DDR binaries to be part of fip-ddr.
    
    Signed-off-by: Pankaj Gupta <pankaj.gupta@nxp.com>

--- plat/nxp/soc-lx2160/ddr_fip.mk
+++ plat/nxp/soc-lx2160/ddr_fip.mk
@@ -4,37 +4,41 @@
 # SPDX-License-Identifier: BSD-3-Clause
 #
 
+DDR_PHY_BIN_PATH	?=	./ddr-phy-binary/lx2160a
+
 ifeq (${DDR_IMEM_UDIMM_1D},)
-    DDR_IMEM_UDIMM_1D	:=	ddr4_pmu_train_imem.bin
+    DDR_IMEM_UDIMM_1D	:=	${DDR_PHY_BIN_PATH}/ddr4_pmu_train_imem.bin
 endif
 
 ifeq (${DDR_IMEM_UDIMM_2D},)
-    DDR_IMEM_UDIMM_2D	:=	ddr4_2d_pmu_train_imem.bin
+    DDR_IMEM_UDIMM_2D	:=	${DDR_PHY_BIN_PATH}/ddr4_2d_pmu_train_imem.bin
 endif
 
 ifeq (${DDR_DMEM_UDIMM_1D},)
-    DDR_DMEM_UDIMM_1D	:=	ddr4_pmu_train_dmem.bin
+    DDR_DMEM_UDIMM_1D	:=	${DDR_PHY_BIN_PATH}/ddr4_pmu_train_dmem.bin
 endif
 
 ifeq (${DDR_DMEM_UDIMM_2D},)
-    DDR_DMEM_UDIMM_2D	:=	ddr4_2d_pmu_train_dmem.bin
+    DDR_DMEM_UDIMM_2D	:=	${DDR_PHY_BIN_PATH}/ddr4_2d_pmu_train_dmem.bin
 endif
 
 ifeq (${DDR_IMEM_RDIMM_1D},)
-    DDR_IMEM_RDIMM_1D	:=	ddr4_rdimm_pmu_train_imem.bin
+    DDR_IMEM_RDIMM_1D	:=	${DDR_PHY_BIN_PATH}/ddr4_rdimm_pmu_train_imem.bin
 endif
 
 ifeq (${DDR_IMEM_RDIMM_2D},)
-    DDR_IMEM_RDIMM_2D	:=	ddr4_rdimm2d_pmu_train_imem.bin
+    DDR_IMEM_RDIMM_2D	:=	${DDR_PHY_BIN_PATH}/ddr4_rdimm2d_pmu_train_imem.bin
 endif
 
 ifeq (${DDR_DMEM_RDIMM_1D},)
-    DDR_DMEM_RDIMM_1D	:=	ddr4_rdimm_pmu_train_dmem.bin
+    DDR_DMEM_RDIMM_1D	:=	${DDR_PHY_BIN_PATH}/ddr4_rdimm_pmu_train_dmem.bin
 endif
 
 ifeq (${DDR_DMEM_RDIMM_2D},)
-    DDR_DMEM_RDIMM_2D	:=	ddr4_rdimm2d_pmu_train_dmem.bin
+    DDR_DMEM_RDIMM_2D	:=	${DDR_PHY_BIN_PATH}/ddr4_rdimm2d_pmu_train_dmem.bin
 endif
+
+$(shell mkdir -p '${BUILD_PLAT}')
 
 ifeq (${DDR_FIP_NAME},)
 ifneq (${TRUSTED_BOARD_BOOT},0)
