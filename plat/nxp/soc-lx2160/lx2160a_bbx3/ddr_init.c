9afcd62154abd6764dd831abe81dd22e48d7977a
Udit Agarwal <udit.agarwal@nxp.com>
2020-09-04 23:04:27 +0530

    driver/ddr/blue_box3: Adds hard-coded settings
    Adds hardcoded setting to configure controller for
    2900Mts and 3200Mts.
    
    Adds the mocked DIMM settings to skip SPD parsing.
    
    Signed-off-by: Udit Agarwal <udit.agarwal@nxp.com>

--- plat/nxp/soc-lx2160/lx2160a_bbx3/ddr_init.c
+++ plat/nxp/soc-lx2160/lx2160a_bbx3/ddr_init.c
@@ -21,29 +21,107 @@
 #include <plat_common.h>
 
 #ifdef CONFIG_STATIC_DDR
-const struct ddr_cfg_regs static_1600 = {
-	.cs[0].config = 0xA8050322,
-	.cs[1].config = 0x80000322,
-	.cs[0].bnds = 0x3FF,
-	.cs[1].bnds = 0x3FF,
-	.sdram_cfg[0] = 0xE5044000,
-	.sdram_cfg[1] = 0x401011,
-	.timing_cfg[0] = 0xFF550018,
-	.timing_cfg[1] = 0xBAB48C42,
-	.timing_cfg[2] = 0x48C111,
-	.timing_cfg[3] = 0x10C1000,
-	.timing_cfg[4] = 0x2,
-	.timing_cfg[5] = 0x3401400,
-	.timing_cfg[7] = 0x13300000,
-	.timing_cfg[8] = 0x2114600,
-	.sdram_mode[0] = 0x6010210,
-	.sdram_mode[8] = 0x500,
-	.sdram_mode[9] = 0x4240000,
-	.interval = 0x18600000,
+const struct ddr_cfg_regs static_3200 = {
+	.cs[0].bnds = 0x07FF,
+	.cs[1].bnds = 0x07FF,
+	.cs[0].config = 0xA8050422,
+	.cs[1].config = 0x80000422,
+	.cs[2].bnds = 0x00,
+	.cs[3].bnds = 0x00,
+	.cs[2].config = 0x00,
+	.cs[3].config = 0x00,
+	.timing_cfg[0] = 0xFFAA0018,
+	.timing_cfg[1] = 0x646A8844,
+	.timing_cfg[2] = 0x00058022,
+	.timing_cfg[3] = 0x13622100,
+	.timing_cfg[4] = 0x02,
+	.timing_cfg[5] = 0x07401400,
+	.timing_cfg[7] = 0x3BB00000,
+	.timing_cfg[8] = 0x0944AC00,
+	.sdram_cfg[0] = 0x65044008,
+	.sdram_cfg[1] = 0x00401011,
+	.sdram_cfg[2] = 0x00,
+	.sdram_mode[0] = 0x06010C50,
+	.sdram_mode[1] = 0x00280400,
+	.sdram_mode[2] = 0x00,
+	.sdram_mode[3] = 0x00,
+	.sdram_mode[4] = 0x00,
+	.sdram_mode[5] = 0x00,
+	.sdram_mode[6] = 0x00,
+	.sdram_mode[7] = 0x00,
+	.sdram_mode[8] = 0x0500,
+	.sdram_mode[9] = 0x10240000,
+	.sdram_mode[10] = 0x00,
+	.sdram_mode[11] = 0x00,
+	.sdram_mode[12] = 0x00,
+	.sdram_mode[13] = 0x00,
+	.sdram_mode[14] = 0x00,
+	.sdram_mode[15] = 0x00,
+	.md_cntl = 0x00,
+	.interval = 0x30C00000,
 	.data_init = 0xDEADBEEF,
+	.init_addr = 0x00,
 	.zq_cntl = 0x8A090705,
+	.sdram_rcw[0] = 0x00,
+	.sdram_rcw[1] = 0x00,
+	.sdram_rcw[2] = 0x00,
+	.sdram_rcw[3] = 0x00,
+	.sdram_rcw[4] = 0x00,
+	.sdram_rcw[5] = 0x00,
+	.err_disable = 0x00,
+	.err_int_en = 0x00,
 };
 
+const struct ddr_cfg_regs static_2900 = {
+	.cs[0].bnds = 0x07FF,
+	.cs[1].bnds = 0x07FF,
+	.cs[0].config = 0xA8050422,
+	.cs[1].config = 0x80000422,
+	.cs[2].bnds = 0x00,
+	.cs[3].bnds = 0x00,
+	.cs[2].config = 0x00,
+	.cs[3].config = 0x00,
+	.timing_cfg[0] = 0xFF990018,
+	.timing_cfg[1] = 0x4F4A4844,
+	.timing_cfg[2] = 0x0005601F,
+	.timing_cfg[3] = 0x125F2100,
+	.timing_cfg[4] = 0x02,
+	.timing_cfg[5] = 0x07401400,
+	.timing_cfg[7] = 0x3AA00000,
+	.timing_cfg[8] = 0x09449B00,
+	.sdram_cfg[0] = 0x65044008,
+	.sdram_cfg[1] = 0x00401011,
+	.sdram_cfg[2] = 0x00,
+	.sdram_mode[0] = 0x06010C50,
+	.sdram_mode[1] = 0x00280400,
+	.sdram_mode[2] = 0x00,
+	.sdram_mode[3] = 0x00,
+	.sdram_mode[4] = 0x00,
+	.sdram_mode[5] = 0x00,
+	.sdram_mode[6] = 0x00,
+	.sdram_mode[7] = 0x00,
+	.sdram_mode[8] = 0x0500,
+	.sdram_mode[9] = 0x10240000,
+	.sdram_mode[10] = 0x00,
+	.sdram_mode[11] = 0x00,
+	.sdram_mode[12] = 0x00,
+	.sdram_mode[13] = 0x00,
+	.sdram_mode[14] = 0x00,
+	.sdram_mode[15] = 0x00,
+	.md_cntl = 0x00,
+	.interval = 0x2C2E0000,
+	.data_init = 0xDEADBEEF,
+	.init_addr = 0x00,
+	.zq_cntl = 0x8A090705,
+	.sdram_rcw[0] = 0x00,
+	.sdram_rcw[1] = 0x00,
+	.sdram_rcw[2] = 0x00,
+	.sdram_rcw[3] = 0x00,
+	.sdram_rcw[4] = 0x00,
+	.sdram_rcw[5] = 0x00,
+	.err_disable = 0x00,
+	.err_int_en = 0x00,
+};
 const struct dimm_params static_dimm = {
 	.rdimm = 0,
 	.primary_sdram_width = 64,
@@ -56,13 +134,13 @@ const struct dimm_params static_dimm = {
 /* Sample code using two UDIMM MT18ASF1G72AZ-2G6B1, on each DDR controller */
 long long board_static_ddr(struct ddr_info *priv)
 {
-	memcpy(&priv->ddr_reg, &static_1600, sizeof(static_1600));
+	memcpy(&priv->ddr_reg, &static_3200, sizeof(static_3200));
 	memcpy(&priv->dimm, &static_dimm, sizeof(static_dimm));
 	priv->conf.cs_on_dimm[0] = 0x3;
 	ddr_board_options(priv);
 	compute_ddr_phy(priv);
 
-	return ULL(0x400000000);
+	return ULL(0x800000000);
 }
 
 #elif defined(CONFIG_DDR_NODIMM)
@@ -72,39 +150,38 @@ long long board_static_ddr(struct ddr_info *priv)
  */
 struct dimm_params ddr_raw_timing = {
 	.n_ranks = 2,
-	.rank_density = 4294967296u,
-	.capacity = 8589934592u,
+	.rank_density = 8589934592u,
+	.capacity = 17179869184u,
 	.primary_sdram_width = 64,
 	.ec_sdram_width = 8,
 	.device_width = 8,
-	.die_density = 0x4,
+	.die_density = 0x5,
 	.rdimm = 0,
 	.mirrored_dimm = 1,
-	.n_row_addr = 15,
+	.n_row_addr = 16,
 	.n_col_addr = 10,
 	.bank_addr_bits = 0,
 	.bank_group_bits = 2,
 	.edc_config = 2,
 	.burst_lengths_bitmask = 0x0c,
-	.tckmin_x_ps = 750,
+	.tckmin_x_ps = 625,
 	.tckmax_ps = 1600,
-	.caslat_x = 0x00FFFC00,
+	.caslat_x = 0x17fffc00,
 	.taa_ps = 13750,
 	.trcd_ps = 13750,
 	.trp_ps = 13750,
 	.tras_ps = 32000,
-	.trc_ps = 457500,
+	.trc_ps = 47500,
 	.twr_ps = 15000,
-	.trfc1_ps = 260000,
-	.trfc2_ps = 160000,
-	.trfc4_ps = 110000,
+	.trfc1_ps = 350000,
+	.trfc2_ps = 260000,
+	.trfc4_ps = 160000,
 	.tfaw_ps = 21000,
-	.trrds_ps = 3000,
+	.trrds_ps = 2500,
 	.trrdl_ps = 4900,
 	.tccdl_ps = 5000,
-	.refresh_rate_ps = 7800000,
+	.refresh_rate_ps = 7800000
 };
-
 int ddr_get_ddr_params(struct dimm_params *pdimm,
 			    struct ddr_conf *conf)
 {
@@ -158,14 +235,18 @@ int ddr_board_options(struct ddr_info *priv)
 		popts->twwt = 0x3;
 		popts->vref_phy = 0x60;	/* 75% */
 		popts->odt = 48;
-		popts->phy_tx_impedance = 48;
+		popts->phy_tx_impedance = 28;
 		break;
 	}
 
 	return 0;
 }
 
+#ifdef NXP_WARM_BOOT
+long long _init_ddr(uint32_t wrm_bt_flg)
+#else
 long long _init_ddr(void)
+#endif
 {
 	int spd_addr[] = { 0x51, 0x52, 0x53, 0x54 };
 	struct ddr_info info;
@@ -200,6 +281,13 @@ long long _init_ddr(void)
 	info.dimm_on_ctlr = DDRC_NUM_DIMM;
 
 	info.warm_boot_flag = DDR_WRM_BOOT_NT_SUPPORTED;
+#ifdef NXP_WARM_BOOT
+	if (wrm_bt_flg) {
+		info.warm_boot_flag = DDR_WARM_BOOT;
+	} else if (wrm_bt_flg == 0x0) {
+		info.warm_boot_flag = DDR_COLD_BOOT;
+	}
+#endif
 
 	dram_size = dram_init(&info
 #if defined(NXP_HAS_CCN504) || defined(NXP_HAS_CCN508)
